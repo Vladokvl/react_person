cf4df3edf7f0576de1e15f33834d728ce9ec0833
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-27 16:54:20 +0200

    done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,16 @@
-export const Person = ({ person }) => {
+export const Person = ({ name, age, sex, isMarried, partnerName }) => {
   const getPartnerText = () => {
-    if (!person.isMarried) return 'not married';
+    if (!isMarried) return 'I am not married';
 
-    return person.sex === 'm'
-      ? `${person.partnerName} is my wife`
-      : `${person.partnerName} is my husband`;
+    return sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`;
   };
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age > 0 ? <p className="Person__age">I am {person.age}</p> : ''}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age > 0 ? <p className="Person__age">I am {age}</p> : ''}
       {getPartnerText() && (
         <p className="Person__partner">{getPartnerText()}</p>
       )}
