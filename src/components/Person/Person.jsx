d2cf9018c903f3deed12a31b1fc05e95ea969b7d
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-27 16:50:54 +0200

    done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const getPartnerText = () => {
+    if (!person.isMarried) return 'not married';
+
+    return person.sex === 'm'
+      ? `${person.partnerName} is my wife`
+      : `${person.partnerName} is my husband`;
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age > 0 ? <p className="Person__age">I am {person.age}</p> : ''}
+      {getPartnerText() && (
+        <p className="Person__partner">{getPartnerText()}</p>
+      )}
+    </section>
+  );
+};
